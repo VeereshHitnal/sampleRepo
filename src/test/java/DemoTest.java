34f450a3aa690d3de8b85c4a41abdf1485c40736
chetanshinnur <chetanshinnur@gmail.com>
2021-12-19 12:18:42 +0530

    Chetan training

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,16 @@
+
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	
+	public void demo() {
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		
+	}
+}
---
+++ src/test/java/FailedTest.java
@@ -0,0 +1,17 @@
+
+
+import static org.testng.Assert.fail;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FailedTest {
+	
+	@Test
+	
+	public void failTest() {
+		System.out.println("Failed");
+		Assert.fail("Failed test case");
+	}
+
+}
